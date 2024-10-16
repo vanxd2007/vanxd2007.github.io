1e22fd4071da3d2b57b98c3d9d48657b4c41a304
vanxd2007 <sd3920178@gmail.com>
2024-10-16 17:07:24 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@ const CACHE_NAME = 'tareas-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
-    '/estilo/vane.css',
-    '/script/vane.js',
-    '/imagen/icon-192x192.png',
-    '/imagen/icon-512x512.png',
+    '/vane.css',
+    '/vane.js',
+    '/imagenes/icon-192x192.png',
+    '/imagenes/icon-512x512.png',
     'https://cdn.jsdelivr.net/bon/bootstrap@5.3.3/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/bon/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
 ];
